26cd3af51320e12000bf9fbafb2e8ccec0b1dd89
henrique <henriquegavabarreto@gmail.com>
2019-04-10 16:45:15 -0400

    add correct guideline link

--- src/stage/fixedGraphics/set-fixed-graphics.js
+++ src/stage/fixedGraphics/set-fixed-graphics.js
@@ -16,8 +16,7 @@ function setFixedGraphics () {
 }
 
 function setGuideline () {
-  //https://henriquegavabarreto.github.io/paraparagame
-  let guideline = PIXI.Sprite.fromImage('/assets/guideline.png')
+  let guideline = PIXI.Sprite.fromImage('https://henriquegavabarreto.github.io/paraparagame/assets/guideline.png')
   guideline.x = 6
   guideline.y = 59
   guideline.scale.x = 0.95
